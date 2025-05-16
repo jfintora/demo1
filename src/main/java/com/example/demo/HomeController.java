af194c6d203660c1c19d5c4bdab27f8f2bb86c36
JFintora <jakub.fintora@gmail.com>
2025-05-16 17:17:38 +0200

    Init

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+class HomeController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home(Model model) {
+        model.addAttribute("msg", "Hello CLB!");
+        return "home";
+    }
+}
